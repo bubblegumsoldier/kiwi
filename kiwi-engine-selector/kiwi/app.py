14863a904d62c34300e723cae3d6769f359c22b4
Martin Essig <mrtnssg@gmail.com>
2017-11-23 22:17:10 +0100

    Adapt to interface of random recommender (which should be matching all other recommenders). Initialize content requests if user has <n unvoted pictures left. All routes and values are still hardcoded.

--- kiwi-engine-selector/kiwi/app.py
+++ kiwi-engine-selector/kiwi/app.py
@@ -1,23 +1,47 @@
 from collections import namedtuple
-
+from json import dumps
 from sanic import Sanic
 from sanic.response import json
 from sanic.request import Request
 from aiohttp import ClientSession
 
-from kiwi.selector.select_recommender import get_pictures
+from kiwi.selector.select_recommender import get_pictures, distribute_content
 
 User = namedtuple('User', 'name')
 
 app = Sanic(__name__)
 
+
 @app.post('/images')
-async def test(request: Request):
+async def images(request: Request):
     post_json = request.json
     user = User(**post_json['user'])
-    with ClientSession() as session:
-        pics = await get_pictures(session, user)
-        return json({"picures": pics})
+    async with ClientSession() as session:
+        response = await get_pictures(session, user)
+
+        if response["unvoted"] < 50:
+            print("Requesting content")
+            await request_content()
+
+        return json({"pictures": response})
+
+
+@app.post('/content')
+async def content(request: Request):
+    post_json = request.json["posts"]
+    async with ClientSession() as session:
+        await distribute_content(session, post_json)
+        return json("OK")
+
+
+async def request_content():
+    data = {"count": 50, "return_url": "http://localhost/8000/content"}
+    async with ClientSession() as session:
+        post = session.post('http://localhost/7000/items', json=dumps(data))
+        async with post as response:
+            res = await response.json()
+            print(res)
+
 
 
 
--- kiwi-engine-selector/kiwi/selector/Recommender.py
+++ kiwi-engine-selector/kiwi/selector/Recommender.py
@@ -3,23 +3,35 @@ from collections import namedtuple
 from aiohttp.client import ClientSession
 
 Voting = namedtuple('Voting', 'user product vote')
-Endpoints = namedtuple('Endpoints', 'pics feedback')
+Endpoints = namedtuple('Endpoints', 'recommend feedback content')
 
 template = '{base}/{endpoint}'
 
-#could have some "is_applicable" method, that gives hints when to use the instance.
+# could have some "is_applicable" method, that gives hints when to use the instance.
+
+
 class Recommender:
     def __init__(self, base_url, endpoints: Endpoints):
         self.base = base_url
         self.endpoints = endpoints
 
+    def _format_template(self, endpoint):
+        return template.format(base=self.base, endpoint=endpoint)
+
     async def get_pics_for_user(self, session: ClientSession, user):
-        url = template.format(base=self.base,
-                              endpoint=self.endpoints.pics)
-        async with session.post(url, json={"user":user.name}) as response:
+        url = self._format_template(self.endpoints.recommend)
+        session = session.get(url,
+                              params={"user": user.name, "count": 10})
+        async with session as response:
             return await response.json()
 
     async def send_feedback(self, session: ClientSession, voting: Voting):
-        url = template.format(base=self.base, endpoint=self.endpoints.feedback)
+        url = self._format_template(self.endpoints.feedback)
         async with session.post(url, json=dumps(voting)):
             return
+
+    async def push_content(self, session: ClientSession, posts):
+        url = self._format_template(self.endpoints.content)
+        async with session.post(url, json=dumps(posts)) as response:
+            res = await response.json()
+            print(res)

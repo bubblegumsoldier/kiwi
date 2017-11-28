a3b4c04f02486d43e6abbeb31e4a556abf30b758
Henry Muessemann <h.muessemann@gmx.de>
2017-11-28 11:11:26 +0100

    Created login screen

--- kiwi-frontend/src/app/login/login.component.ts
+++ kiwi-frontend/src/app/login/login.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  private username :string = "";
+  private loading :boolean = false;
+
+
   constructor() { }
 
   ngOnInit() {
+    this.stopLoading();
   }
 
+  onRegister()
+  {
+    console.log("Register " + this.username);
+    this.startLoading();
+  }
+
+  onLogin()
+  {
+    console.log("Login " + this.username);
+    this.startLoading();
+  }
+
+  private isLoading()
+  {
+    return this.loading;
+  }
+
+  private startLoading()
+  {
+    this.loading = true;
+  }
+
+  private stopLoading()
+  {
+    this.loading = false;
+  }
 }

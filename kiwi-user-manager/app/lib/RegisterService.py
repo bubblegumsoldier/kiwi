71f0f30149ce19de50aa6693d251d636cb2bbd50
Martin Essig <mrtnssg@gmail.com>
2017-12-05 01:27:16 +0100

    Rename to fit docker-nginx image. Change return content-type header. Add error if username is not valid during register.

--- kiwi-user-manager/app/lib/RegisterService.py
+++ kiwi-user-manager/app/lib/RegisterService.py
@@ -1,4 +1,6 @@
 from lib.username_validator import validate
+from werkzeug.exceptions import abort
+from http import HTTPStatus
 
 class RegisterService(object):
     def __init__(self, user_database_connection):
@@ -12,6 +14,6 @@ class RegisterService(object):
 
     def register(self, username):
         if not validate(username):
-            return False
+            abort(HTTPStatus.NOT_ACCEPTABLE)
             
         return self.user_database_connection.insert(username)

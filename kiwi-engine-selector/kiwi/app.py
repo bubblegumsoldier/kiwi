5a1dba4f99996c37b0e0ad1f6c3931e003a5af5c
Martin Essig <mrtnssg@gmail.com>
2017-12-06 19:43:56 +0100

    Closes T2
    The engine-selector service can now access the content database (mongo) and return metadata for posts.
    Currently the response for a recommendation is enriched into the shape {id, title, src}.
    Where src is the direct link to the image.

--- kiwi-engine-selector/kiwi/app.py
+++ kiwi-engine-selector/kiwi/app.py
@@ -8,6 +8,7 @@ from kiwi.config import APP_CONFIG, CONTENT_CONFIG, RECOMMENDERS
 from kiwi.selector.Types import User, Voting
 from kiwi.selector.RecommenderSelector import RecommenderSelector
 from kiwi.enricher.Enricher import Enricher
+from kiwi.database.MongoConnection import get_connection
 
 app = Sanic(__name__)
 selector = RecommenderSelector.from_config(RECOMMENDERS)
@@ -17,44 +18,53 @@ selector = RecommenderSelector.from_config(RECOMMENDERS)
 async def images(request: Request):
     post_json = request.json
     user = User(**post_json['user'])
-    async with ClientSession() as session:
-        response = await selector.get_recommendations(session, user)
-        if response.json['unvoted'] <= CONTENT_CONFIG['unvoted_threshold']:
-            await request_content()
-        return json({'recommendations': {
-            'user': response.json['user'],
-            'posts': await Enricher().enrich(response.json['posts'])}})
+    response = await selector.get_recommendations(app.client_session, user)
+    if response.json['unvoted'] <= CONTENT_CONFIG['unvoted_threshold']:
+        await request_content()
+    return json({'recommendations': {
+        'user': response.json['user'],
+        'posts': await Enricher(app.mongo_connection).enrich(
+            response.json['posts'])}})
 
 
 @app.post('/content')
 async def content(request: Request):
-    async with ClientSession() as session:
-        await selector.distribute_posts(session, request.json['posts'])
-        return json({'accepted': True})
+    await selector.distribute_posts(app.client_session, request.json['posts'])
+    return json({'accepted': True})
 
 
 @app.post('/feedback')
 async def feedback(request: Request):
     post_json = request.json
     voting = Voting(**post_json['feedback'])
-    async with ClientSession() as session:
-        await selector.distribute_vote(session, voting)
-        return json({'accepted': True})
+    await selector.distribute_vote(app.client_session, voting)
+    return json({'accepted': True})
 
 
 async def request_content():
     data = {'count': CONTENT_CONFIG['unvoted_threshold'],
             'return_url': build_url(**APP_CONFIG, endpoint='content')}
-    async with ClientSession() as session:
-        url = build_url(**CONTENT_CONFIG['address'])
-        
-        post = session.post(url, json=data)
-        async with post as response:
-            await response.text()
+    url = build_url(**CONTENT_CONFIG['address'])
+
+    post = app.client_session.post(url, json=data)
+    async with post as response:
+        await response.text()
+
 
 def build_url(**kwargs):
     return 'http://{host}:{port}/{endpoint}'.format_map(kwargs)
 
 
-if __name__ == '__main__':
+@app.listener('before_server_start')
+def init(app, loop):
+    app.mongo_connection = get_connection()
+    app.client_session = ClientSession(loop=loop)
+
+
+@app.listener('after_server_stop')
+async def teardown(app, loop):
+    app.mongo_connection.close()
+    await app.client_session.close()
+
+if __name__ == '__main__': 
     app.run(host='0.0.0.0', port=APP_CONFIG['port'])

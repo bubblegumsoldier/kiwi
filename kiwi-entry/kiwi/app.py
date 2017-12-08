c9c907d981c55c0d3d587aa9e4fdcbf2ec5a77e1
Martin Essig <mrtnssg@gmail.com>
2017-12-08 14:26:29 +0100

    Add route for standalone authentication to kiwi-entry

--- kiwi-entry/kiwi/app.py
+++ kiwi-entry/kiwi/app.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 from sanic import Sanic
-from sanic.response import json 
+from sanic.response import json
 from sanic.request import Request
 from sanic.exceptions import abort
 from aiohttp import ClientSession
@@ -23,6 +23,7 @@ recommender_router = RecommenderRouter(
 def init(app: Sanic, loop):
     app.http_session = ClientSession(loop=loop)
 
+
 @app.listener('after_server_stop')
 async def teardown(app, loop):
     await app.http_session.close()
@@ -30,7 +31,7 @@ async def teardown(app, loop):
 
 @app.get('/recommendation/<user>')
 @app.get('/recommendation/<user>/<count>')
-async def recommendation(request, user, count=10):
+async def recommendation(request, user: str, count: int=10):
     is_valid_user = await user_manager.authenticate_user(user, app.http_session)
     if is_valid_user:
         response = await recommender_router.recommend(app.http_session, user, count)
@@ -45,17 +46,30 @@ async def feedback(request: Request):
     user = request.json['feedback']['user']
     is_valid_user = await user_manager.authenticate_user(user, app.http_session)
     if is_valid_user:
-        response = await recommender_router.feedback(app.http_session, request.json)
+        await recommender_router.feedback(app.http_session, request.json)
         return json({}, status=HTTPStatus.ACCEPTED)
 
-@app.post('/user/<user>')
-async def register(request, user):
+
+@app.route('/users/<user>', methods=['GET', 'POST'])
+async def user(request: Request, user: str):
     if not user:
         return json({'error': 'no username provided'}, HTTPStatus.BAD_REQUEST)
+
+    if request.method == "GET":
+        return json({'user': user}, await handle_users_get(user))
+    return json({'user': user}, await handle_users_post(user))
+
+
+async def handle_users_get(user):
+    authenticated = await user_manager.authenticate_user(user,
+                                                         app.http_session)
+    return HTTPStatus.OK if authenticated else HTTPStatus.UNAUTHORIZED
+
+
+async def handle_users_post(user):
     registration = await user_manager.register_user(user, app.http_session)
-    if registration:
-        return json({'user': user}, HTTPStatus.CREATED)
-    return json({'user': user}, HTTPStatus.CONFLICT)
+    return HTTPStatus.CREATED if registration else HTTPStatus.CONFLICT
+    
 
 
 if __name__ == '__main__':

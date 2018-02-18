87d6b88e298269561dd7003c3dcc914808ac1c18
Martin Essig <mrtnssg@gmail.com>
2018-02-18 22:20:10 +0100

    Implement rudimentary periodic retraining with the movielens dataset and document a few peculiarities

--- kiwi-surprise/kiwi-surprise-app/TransferTypes.py
+++ kiwi-surprise/kiwi-surprise-app/TransferTypes.py
@@ -1,3 +1,14 @@
 from collections import namedtuple
 
 Vote = namedtuple('Vote', 'user post vote')
+
+
+def create_vote(vote_dict):
+    """
+    The movielens algorithm expects integer user and items ids.
+    This function adapts the strings in a vote accordingly.
+    """
+    user = int(vote_dict['user'])
+    post = int(vote_dict['post'])
+    vote = float(vote_dict['vote'])
+    return Vote(user=user, post=post, vote=vote)

ff66b0a923669692082da7b947a4d256d531a367
Martin Essig <mrtnssg@gmail.com>
2018-02-18 18:46:35 +0100

    Test recommender service with surprise algorithms. Does not support retraining or unkown users (yet)
    The data accessor is currently not using a database, but the interface should be reuseable for that case.
    Instead the Movielens 100k dataset is currently used.
    Training this set currently takes a few seconds.
    
    Recommendation times take ~3 seconds, while being parallelized with multiple processes.
    (4 cores).
    Part of the time taken is the access of the n top rateditems.
    Caching and or stored procedures could improve these times.

--- kiwi-surprise/kiwi-surprise-app/Recommender.py
+++ kiwi-surprise/kiwi-surprise-app/Recommender.py
@@ -1,44 +1,35 @@
-import algorithm_persistence_tools
-import os
-from surprise import BaselineOnly
-from surprise import Dataset
-from surprise import Reader
-import csv_reader
+from Algorithm import Algorithm
+from database import DataAccessor
+
 
 class Recommender:
-    def __init__(self, data_accessor):
-        self.accessor = data_accessor
+    def __init__(self, algorithm: Algorithm, data_accessor: DataAccessor):
+        self._algo = algorithm
+        self._accessor = data_accessor
 
     async def recommend_for(self, user, count=10):
-        algorithm = algorithm_persistence_tools.load_algorithm()
-        data = await self.get_data()
-        trainset = data.build_full_trainset()
-        algorithm.predict()
-        return await self.accessor.recommend_for(user, count)
-
-    async def get_data(self):
-        # path to dataset file
-        file_path = csv_reader.get_data_path()
-
-        # As we're loading a custom dataset, we need to define a reader. In the
-        # movielens-100k dataset, each line has the following format:
-        # 'user item rating timestamp', separated by '\t' characters.
-        reader = Reader(line_format='user item rating timestamp', sep='\t')
-        data = Dataset.load_from_file(file_path, reader=reader)
-        return data
+        await self._register_user(user)
+        items = await self._accessor.get_unvoted_items(user)
+        recommendations = await self._algo.get_top_n_items(user, items, count)
+        unvoted = len(items)
+        return {
+            'posts': [r[0] for r in recommendations],
+            'user': user,
+            'unvoted': unvoted
+        }
 
     async def store_feedback(self, vote):
         await self._register_user(vote.user)
-        success = await self.accessor.store_feedback(vote)
-        unvoted = await self.accessor.get_unvoted_count(vote.user)
+        success = await self._accessor.store_feedback(vote)
+        unvoted = await self._accessor.get_unvoted_count(vote.user)
         if success:
             return {'user': vote.user, 'post': vote.post, 'unvoted': unvoted}
         return {}
 
     async def add_content(self, posts):
         filtered_posts = [post['id'] for post in posts]
-        inserted = await self.accessor.add_content(filtered_posts)
+        inserted = await self._accessor.add_content(filtered_posts)
         return {'inserted_count': inserted}
 
     async def _register_user(self, user):
-        await self.accessor.check_and_register_user(user)
+        await self._accessor.check_and_register_user(user)
--- kiwi-surprise/kiwi-surprise-app/config.py
+++ kiwi-surprise/kiwi-surprise-app/config.py
@@ -1,14 +1,15 @@
 import os
+from surprise import KNNWithMeans
 
 def create_algorithm():
     """
     See: http://surprise.readthedocs.io/en/stable/prediction_algorithms.html
     """
-    sim_options = {
-        'name': 'cosine',
-        'user_based': False
-    }
-    algo = KNNBasic(sim_options=sim_options)
+    # sim_options = {
+    #     'name': 'cosine',
+    #     'user_based': False
+    # }
+    algo = KNNWithMeans(min_k=0, k=40,sim_options={'user_based': False})
     return algo
 
 _dir_path = os.path.dirname(os.path.realpath(__file__))

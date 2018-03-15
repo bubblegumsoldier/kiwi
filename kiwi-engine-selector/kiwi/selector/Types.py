28759636377d3d3ed0dfca278af7de043165a060
Henry Muessemann <h.muessemann@gmx.de>
2018-03-15 12:48:42 +0100

    added activation to type

--- kiwi-engine-selector/kiwi/selector/Types.py
+++ kiwi-engine-selector/kiwi/selector/Types.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
 Voting = namedtuple('Voting', 'user post vote')
-Endpoints = namedtuple('Endpoints', 'recommendation feedback content')
+Endpoints = namedtuple('Endpoints', 'recommendation feedback content activation')
 RecommendationRequest = namedtuple('RecommendationRequest', 'user count')
 Response = namedtuple('Response', 'status json')

56bd800379fe645f5817968069b8ebfe98def7dd
Martin Essig <mrtnssg@gmail.com>
2018-04-09 15:20:26 +0200

    Add predict endpoint and cleanup config

--- kiwi-engine-selector/kiwi/selector/Recommender.py
+++ kiwi-engine-selector/kiwi/selector/Recommender.py
@@ -1,35 +1,45 @@
 from aiohttp.client import ClientSession
-from kiwi.selector.Types import Voting, Endpoints, Response, RecommendationRequest
+from kiwi.selector.Types import Voting, Response, RecommendationRequest
 
 TEMPLATE = '{base}/{endpoint}'
 
 
 class Recommender:
-    def __init__(self, host, port, endpoints: Endpoints):
-        self.base = 'http://{}:{}'.format(host, port)
-        self.endpoints = endpoints
+    def __init__(self, address):
+        self.base = address
 
     @classmethod
-    def from_config(cls, config_dict):
-        endpoints = Endpoints(**config_dict['endpoints'])
-        self = Recommender(**config_dict['address'], endpoints=endpoints)
+    def from_config(cls, address):
+        self = Recommender(address)
         return self
 
     async def get_content_for_user(self, session: ClientSession,
                                    request: RecommendationRequest):
-        url = self._format_template(self.endpoints.recommendation)
+        url = self._format_template('recommendation')
         session = session.get(url, params=request._asdict())
         return await self._get_response(session)
 
+    async def predict_for(self, session, user, item):
+        url = self._format_template('prediction')
+        response = session.get(url, params={'user': user, 'item': item})
+        return await self._get_response(response)
+
     async def send_feedback(self, session: ClientSession, voting: Voting):
-        url = self._format_template(self.endpoints.feedback)
+        url = self._format_template('feedback')
         session = session.post(url, json={'vote': voting._asdict()})
         return await self._get_response(session)
 
     async def push_content(self, session: ClientSession, posts):
-        url = self._format_template(self.endpoints.content)
+        url = self._format_template('content')
         session = session.post(url, json={'posts': posts})
         return await self._get_response(session)
+
+    async def get_activation(self, session: ClientSession, heuristics):
+        url = self._format_template('activation')
+        param_dict = dict(heuristics=heuristics)
+        session = session.get(url, json=param_dict)
+        response = await self._get_response(session)
+        return response.json["activation"]
 
     async def _get_response(self, session):
         async with session as response:
@@ -37,13 +47,6 @@ class Recommender:
                 return Response(status=response.status, json={})
             return Response(status=response.status,
                             json=await response.json())
-
-    async def get_activation(self, session: ClientSession, heuristics):
-        url = self._format_template(self.endpoints.activation)
-        param_dict = dict(heuristics=heuristics)
-        session = session.get(url, json=param_dict)
-        response = await self._get_response(session)
-        return response.json["activation"]
 
     def _format_template(self, endpoint):
         return TEMPLATE.format(base=self.base, endpoint=endpoint)
--- kiwi-engine-selector/kiwi/selector/recommender_distribution.py
+++ kiwi-engine-selector/kiwi/selector/recommender_distribution.py
@@ -2,32 +2,25 @@ from logging import getLogger
 from kiwi.selector.Types import Response
 from asyncio import gather
 
-class ContentException(Exception):
-    pass
-
 
 async def content(session, recommenders, posts):
     """
-    What happens if the failing recommender is the middle one?
-    First one will have received updates, second one errors, third one nothing?
-    -> Inconsistent.
-    For concistency needs some kind of rollback, but that will likely be difficult to implement.
-    Other option: Fail softly? I.e. do not complain on duplicates?
-    Also: Could be optimized to start all requests in parallel.
+    Distribute content across all recommenders.
+    If any recommender fails (i.e. not 200), the returned status code will be 500
     """
+    results = await gather([_content(session, name, recommender, posts)
+                            for name, recommender in recommenders])
+    return all([True if status == 200 else False for status in results])
 
-    results = await gather([_content(session, name, recommender, posts) 
-            for name, recommender in recommenders])
-    return all([True if status == 200 else False for status in results])        
 
 async def feedback(session, recommenders, voting):
-    for _, recommender in recommenders.items():
-        await recommender.send_feedback(session, voting)
+    await gather(
+        [r.send_feedback(session, voting) for r in recommenders.values()])
 
 
 async def _content(session, name, recommender, posts):
     response = await recommender.push_content(session, posts)
     if response.status == 500:
         getLogger("error").error(
-            "Internal Server Error for recommender: %s", name)        
+            "Internal Server Error for recommender: %s during content distribution", name)
     return response.status
